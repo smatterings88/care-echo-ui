145aa5aa8093888335f878492bad20ed89612af6
sMATTerings <107927883+smatterings88@users.noreply.github.com>
2025-09-03 09:19:35 +0800

    feat: hide floating action button on landing page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import FloatingActionButton from "@/components/FloatingActionButton";
+// import FloatingActionButton from "@/components/FloatingActionButton";
 
 const Index = () => {
   return (
@@ -49,7 +49,7 @@ const Index = () => {
       </section>
 
       <Footer />
-      <FloatingActionButton />
+      {/* <FloatingActionButton /> */}
     </div>
   );
 };
